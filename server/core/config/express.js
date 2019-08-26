91bf814e58207e129092cfd140b035c8aa598c24
aanchalahuja1 <aanchal.ahuja1@renovite.com>
2019-08-26 21:05:44 +0530

    cors issue resolved

--- server/core/config/express.js
+++ server/core/config/express.js
@@ -19,7 +19,7 @@ module.exports = function (app) {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
   app.use(fileUpload());
-  app.use(cors())
+  app.use(cors({credentials:true, origin: true}))
   //app.set('views', path.join(__dirname, '../../../dist'))
   //app.use('/', express.static(path.join(__dirname, '../../../dist')))
 }
