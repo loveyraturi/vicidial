a9f0cf9672ed727974ebc884b6d840c1d599936e
aanchalahuja1 <aanchal.ahuja1@renovite.com>
2019-09-02 17:27:08 +0530

    Made changes for MCC multiple values from backend

--- server/core/model/feature.js
+++ server/core/model/feature.js
@@ -26,7 +26,9 @@ module.exports.findFeatureList = function () {
   .select()
   .from('feature').as('feature')
   .orderBy('id', 'desc')
-  .where({type: 'non-functional' })
+  .where({type: 'non-functional' ,
+  parentfield: 'MCC'
+})
 }
 module.exports.findActiveFeatureList = function (status) {
   return global.db.column([
